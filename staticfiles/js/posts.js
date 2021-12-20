53e8725071cd6cfb618243b916aef3effc4e7f1b
Teshawn9 <teshawn.kelly@yahoo.com>
2021-12-19 22:49:48 -0600

    night

---
+++ staticfiles/js/posts.js
@@ -0,0 +1,13 @@
+//////////////////////////
+// JavaScript for Posts Page
+///////////////////////////
+
+$(function () {
+  // Executed when js-menu-icon is clicked
+  $(".js-menu-icon").click(function () {
+    //$(this): self element, namely div.js-menu-icon
+    //next(): next to div.js-menu-icon, namely div.menu
+    //toggle(): Switch show and hide
+    $(this).next().toggle();
+  });
+});
